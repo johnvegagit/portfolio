3a4584553981775edb75dfee80865fabbae11bff
johnvegagit <johnvegagit@outlook.com>
2024-03-09 19:43:25 -0600

    add portfolio

--- app/template/header.php
+++ app/template/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/public_html/portfolio/public/assets/style/index.css">
-    <script src="http://localhost/public_html/portfolio/public/assets/script.js/index.js" defer></script>
+    <script src="http://localhost/public_html/portfolio/public/assets/script/index.js" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>john vega | porfolio</title>
 </head>
